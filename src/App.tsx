7b67417d3e1697155bdac5dd69fe6e71f2b800d4
Mariano Capiliku <marianocapiliku@gmail.com>
2025-06-28 12:44:20 +0100

    created Badge component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Badge from "./components/badge";
 import Text from "./components/text";
 
 function App() {
-  return <Text variant="body-md-bold">Todo List</Text>;
+  return (
+    <div>
+      <Text> Marinao</Text>
+
+      <Badge variant="secondary">5</Badge>
+      <Badge variant="primary">2 de 5</Badge>
+    </div>
+  );
 }
 
 export default App;
