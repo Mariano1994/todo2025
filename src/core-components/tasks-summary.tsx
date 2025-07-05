9865a08b420e1365c53b563fd685665e144954df
Mariano Capiliku <marianocapiliku@gmail.com>
2025-07-05 23:49:54 +0100

    created loading state on tasks-summary component

--- src/core-components/tasks-summary.tsx
+++ src/core-components/tasks-summary.tsx
@@ -1,9 +1,10 @@
 import Badge from "../components/badge";
+import Skeleton from "../components/skeleton";
 import Text from "../components/text";
 import useTasks from "../hooks/use-tasks";
 
 function TasksSummary() {
-  const { tasksCount, concludedTasksCount } = useTasks();
+  const { tasksCount, concludedTasksCount, isLoadingTasks } = useTasks();
   return (
     <div className="flex justify-between items-center">
       <div className="flex items-center gap-2">
@@ -11,7 +12,11 @@ function TasksSummary() {
           {" "}
           Tarefas Criadas
         </Text>
-        <Badge variant={"secondary"}>{tasksCount}</Badge>
+        {isLoadingTasks ? (
+          <Skeleton rounded="full" className="h-6 w-6" />
+        ) : (
+          <Badge variant={"secondary"}>{tasksCount}</Badge>
+        )}
       </div>
 
       <div className="flex items-center gap-2">
@@ -19,9 +24,14 @@ function TasksSummary() {
           {" "}
           Concluidas
         </Text>
-        <Badge variant={"primary"}>
-          {concludedTasksCount} de {tasksCount}
-        </Badge>
+
+        {isLoadingTasks ? (
+          <Skeleton rounded="full" className="h-6 w-6" />
+        ) : (
+          <Badge variant={"primary"}>
+            {concludedTasksCount} de {tasksCount}
+          </Badge>
+        )}
       </div>
     </div>
   );
